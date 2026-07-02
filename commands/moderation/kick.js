1bdc2ef47b534e31f61e28138fa883141adc572f
JauniKapauni <riches-goods.54@icloud.com>
2026-07-02 12:29:40 +0200

    feat: add command to kick user

---
+++ commands/moderation/kick.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder().setName('kick').setDescription('Kick a member from the server').addUserOption(option => option.setName('user').setDescription('The user to kick').setRequired(true)).addStringOption(option => option.setName('reason').setDescription('Reason for the kick').setRequired(true)).setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+    async execute(interaction){
+        const member = interaction.options.getMember('user');
+        const reason = interaction.options.getString('reason');
+        if(!member){
+            return interaction.reply({
+                content: 'User not found in this server',
+                ephemeral: true,
+            });
+        }
+        if(!member.kickable){
+            return interaction.reply({
+                content: 'User cannot be kicked',
+                ephemeral: true,
+            });
+        }
+        try{
+            await member.kick(reason);
+            await interaction.reply(`${member.user.tag} was kicked for ${reason}`);
+        } catch (e){
+            console.error(e);
+            await interaction.reply({
+                content: `Failed to kick the user`,
+                ephemeral: true
+            })
+        }
+    }
+}
